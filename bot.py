0d3556052279cc4224617532ae64181f92106fce
daanwillems <dwillems2@avans.nl>
2017-10-03 16:13:53 +0200

    Suicide

--- bot.py
+++ bot.py
@@ -17,6 +17,11 @@ async def woop(ctx):
     """Woop woop"""
     await bot.say(ctx.message.author.display_name+ " is excited!")
 
+@bot.command(pass_context=True)
+async def kill(ctx):
+    """Make the bot commit suicide"""
+    await bot.say("Go fuck yourself "+ctx.message.author.display_name+". You monster...")
+
 @bot.command()
 async def repeat():
     """Woop woop"""
